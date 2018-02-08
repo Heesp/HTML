5bb7ae9d250a7ee3f138fecddec1f025eea84746
Yaroslav Kostiukov <35146266+Heesp@users.noreply.github.com>
2018-02-08 12:09:44 -0800

    CV_2.0 first commit

---
+++ CV_2.0/menu.js
@@ -0,0 +1,10 @@
+const blockTrigger = document.querySelector('.block')
+
+
+console.log(blockTrigger);
+
+blockTrigger.addEventListener('click',function(){
+
+  console.log("Eureka!");
+  // this.toggle('center')
+});
